5e0380482c491d34ce02461ac0e9b92463b5fccb
BilalBeouche <beouchebilal91@gmail.com>
2025-07-20 18:01:14 +0200

    Ajout de la class Vehicule

---
+++ backend/src/main/java/com/clean/auto/backend/entity/Vehicule.java
@@ -0,0 +1,89 @@
+package com.clean.auto.backend.entity;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "vehicules")
+public class Vehicule {
+
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToAny
+    @JoinColumn(name = "users_id")
+    private Users users;
+
+    private String type;
+
+    private String marque;
+
+    private String modele;
+
+    public Vehicule() {
+    }
+
+    public Vehicule(Users users, String type, String marque, String modele) {
+        this.users = users;
+        this.type = type;
+        this.marque = marque;
+        this.modele = modele;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMarque() {
+        return marque;
+    }   
+
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    } 
+
+    public String getModele() {
+        return modele;
+    }
+     
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+    
+
+
+
+    
+
+
+    
+
+    
+}
