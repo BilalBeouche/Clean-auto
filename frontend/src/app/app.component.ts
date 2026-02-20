ca3771438ad8da19c5e585d4ce9ad77ebff9fcf1
BilalBeouche <beouchebilal91@gmail.com>
2026-02-20 14:10:54 +0100

    MAJ front

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from "./header/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,           // <-- important pour Angular standalone
-  imports: [RouterModule, NavbarComponent],     // <-- permet d'utiliser routerLink et routerLinkActive
+  imports: [RouterModule, NavbarComponent, FooterComponent],     // <-- permet d'utiliser routerLink et routerLinkActive
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
